b74a1dd7e80dc1281e938ac03fca416088db350b
Padraig <C13329991@mydit.ie>
2015-09-30 16:55:36 +0100

    implemented check to see if entered word is the same as its reversed form

--- Lab3/palindrome.py
+++ Lab3/palindrome.py
@@ -7,3 +7,8 @@ entered_word = raw_input("Enter a word: ")
 # Reverse the entered word
 rev_word = reversed(entered_word)
 
+# Compare the strings
+if list(entered_word) == list(rev_word):
+	print("True")
+else:
+	print("False")
